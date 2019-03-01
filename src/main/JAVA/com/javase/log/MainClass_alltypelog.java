d1b3d6bd41ec1a0b5f7a71ddac2fd447cac156a6
allen <353263668@qq.com>
2019-03-01 14:31:29 +0800

    Update MainClass_alltypelog.java

--- src/main/JAVA/com/javase/log/MainClass_alltypelog.java
+++ src/main/JAVA/com/javase/log/MainClass_alltypelog.java
@@ -58,6 +58,6 @@ public class MainClass_alltypelog {
     }
 
     public void test20190301(){
-        System.out.println("更新20190301 13:17");
+        System.out.println("更新20190301 14:19");
     }
 }
